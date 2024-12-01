ccd7db4c93f765b4859c1021c756f47c2935e887
jun-dailybasis <23322388+jun-dailybasis@users.noreply.github.com>
2024-12-01 20:42:58 +0900

    icon, component modified

--- news-web-app/src/components/Paperbase.tsx
+++ news-web-app/src/components/Paperbase.tsx
@@ -14,7 +14,7 @@ function Copyright() {
     <Typography variant="body2" align="center" sx={{ color: 'text.secondary' }}>
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        Jun-Young Bang (20249184)
       </Link>{' '}
       {new Date().getFullYear()}.
     </Typography>
